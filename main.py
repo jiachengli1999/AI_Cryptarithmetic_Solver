4a7e3f33c045e4279e1d93706ed20d996d2ff772
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2019-12-12 20:30:17 -0500

    added comments

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import copy
 
+# keeps track of the positions (1-13) and domain(0-9) of a character (e.g. x1)
 class Node():
     def __init__(self, char):
         self.char = char
         self.positions = []
         self.domain = []
 
+# help keep track of the current assigned values and carry overs
 class State():
     def __init__(self):
         self.values = []
@@ -15,6 +17,7 @@ class State():
         self.C3 = None
         self.C4 = None
 
+# make a dict with the position as the key from a character_dict (char as key)
 def make_position_dict(char_dict):
     postion_dict = {}
     for key, val in char_dict.items():
@@ -23,11 +26,12 @@ def make_position_dict(char_dict):
                 postion_dict[i] = val.char
     return postion_dict
 
+# define the appropriate domain for each char
 def assign_domain(s1, s2, s3):
     print(s1, s2, s3)
     print(len(s1), len(s2), len(s3))
     char_dict = {}
-    # make a dict for each char
+    # make a dict for each char containing its positions
     str_lst = [s1, s2, s3]
     counter = 1
     for word in str_lst:
@@ -38,43 +42,31 @@ def assign_domain(s1, s2, s3):
             counter += 1
     # get domains for each char
     for char, node in char_dict.items():
+        # only domain for x9 is 1 
         if 9 in node.positions:
             node.domain.append(1)
         else:
             start_domain = 0
+            # 1 and 5 are leading terms, so domain: 1-9
             if 1 in node.positions or 5 in node.positions:
-                # domain 1-9 for x1 and x5
-                start_domain = 1
+                start_domain = 1 # start at 1
             for i in range(start_domain, 10):
                 node.domain.append(i)
     # make position dict
     position_dict = make_position_dict(char_dict)
     # assign edge case domains
-    edge_positions = [10, 11, 12, 13]
+    edge_positions = [10, 13]
     for num in edge_positions:
+        # if previous 2 terms are same char then their sum gives even domain
         if position_dict[num-5] == position_dict[num-9]:
             char = position_dict[num]
-            if num == 10:
-                char_dict[char].domain = [0, 2, 4, 6, 8]
-    # if duplicate leading letters then domain is [5, 6, 7, 8, 9]
-    if position_dict[5] == position_dict[1]:
-        char = position_dict[1]
-        # if duplicate leading letters are even 
-        if len(char_dict[char].domain) == 5:
-            dup_domain = [0, 2, 4, 6, 8]
-            flag = True
-            for i in dup_domain:
-                if i not in char_dict[char].domain:
-                    flag = False
-                    char_dict[char].domain = [5, 6, 7, 8, 9]
-                    break
-            if flag: # since C4 = 1, and x1 = x5 and they are even x1 + x5 >= 10 for [6, 8]
-                char_dict[char].domain = [6, 8]
-        else:
-            char_dict[char].domain = [5, 6, 7, 8, 9]
+            char_dict[char].domain = [0, 2, 4, 6, 8] 
+ 
     return char_dict
 
 # true if val is not already assigned in state
+# check if the number works out in the addition
+# if work then use this func again with modify = 1 to modify state
 def can_add_val(state, val, char_dict, char, modify):
     # False if val is already used by a letter 
     if val in state.values:
@@ -82,20 +74,13 @@ def can_add_val(state, val, char_dict, char, modify):
     positions = char_dict[char].positions
     values = copy.deepcopy(state.values)
     C1, C2, C3, C4 = state.C1, state.C2, state.C3, state.C4
-    # for i in state.values:
-    #     values.append(i)
     for p in positions:
         values[p-1] = val
     
-    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13 = values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9], values[10], values[11], values[12]
-    # x4 + x8 = x13 + C1*10
-    # x3 + x7 + C1 = x12 + C2*10
-    # x2 + x6 + C2 = x11 + C3*10
-    # x1 + x5 + C3 = x10 + C4*10
-    # C4 = x9
-    
-    # assign C values
-    # print(1)
+    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13 = \
+    values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9], values[10], \
+    values[11], values[12]
+
     if x4 != -1 and x8 != -1 and x13 != -1:
         val = x4 + x8
         if val >= 10:
@@ -104,7 +89,6 @@ def can_add_val(state, val, char_dict, char, modify):
             C1 = 0
         if x4 + x8 != x13 + C1*10:
             return False
-    # print(2)
     if x3 != -1 and x7 != -1 and x12 != -1:
         # if C1 is not calculated then test for C1 = 0 and C1 = 1
         # return False if fail for both
@@ -121,7 +105,7 @@ def can_add_val(state, val, char_dict, char, modify):
             if x3 + x7 + C1 != x12 + C2*10:
                 flag = False
             if not flag:
-                # c1 == 1
+                # test when c1 == 1
                 C1 = 1
                 val = x3 + x7 + C1
                 if val >= 10: 
@@ -138,8 +122,6 @@ def can_add_val(state, val, char_dict, char, modify):
                 C2 = 0
             if x3 + x7 + C1 != x12 + C2*10: 
                 return False
-        
-    # print(3)
     if x2 != -1 and x6 != -1 and x11 != -1:
         # if C1 is not calculated then test for C1 = 0 and C1 = 1
         # return False if fail for both
@@ -171,7 +153,6 @@ def can_add_val(state, val, char_dict, char, modify):
                 C3 = 0
             if x2 + x6 + C2 != x11 + C3*10: 
                 return False
-    # print(4)
     if x1 != -1 and x5 != -1 and x10 != -1:
         # if C1 is not calculated then test for C1 = 0 and C1 = 1
         # return False if fail for both
@@ -204,18 +185,17 @@ def can_add_val(state, val, char_dict, char, modify):
             if x1 + x5 + C3 != x10 + C4*10: 
                 return False
         # C4 == x9 == 1
-        # print(5)
         if C4 == 0: 
             return False
     
-    # print(6)
     if modify:
         state.values = values
         print('values')
         print(state.values)
+        # modify values in state
         state.C1, state.C2, state.C3, state.C4 = C1, C2, C3, C4
-        # remove char from res_dict
         print('CHAR:', char)
+        # add char to assigned 
         state.assigned.add(char)
         print('assigned')
         print(state.assigned)
@@ -229,7 +209,7 @@ def get_empty_neighbors(char, state, char_dict):
     min_num = None
     # check empty neighbors for each position 
     positions = char_dict[char].positions
-    for i in positions:
+    for i in positions: # positions from 1-13
         num_empty = 0
         # current position in state: state.values[i-1]
         if i - 1 > 0:
@@ -336,19 +316,21 @@ def check(state):
 # done if positions are all assigned and values are all uniq
 def done(state):
     values = state.values
+    # all values have to be assigned, e.g. not = -1
     for i in values:
         if i == -1:
             return False
     # check if satisfy constraint 
     return check(state)
 
-def remove_position(position_dict, position):
-    new_dict = {}
-    for key, val in position_dict.items():
-        if key != position: 
-            new_dict[key] = val
-    return new_dict
+# def remove_position(position_dict, position):
+#     new_dict = {}
+#     for key, val in position_dict.items():
+#         if key != position: 
+#             new_dict[key] = val
+#     return new_dict
 
+# change modify values back
 def remove_assigned(state, char, positions):
     state.assigned.remove(char)
     # reset the values to -1
@@ -369,7 +351,7 @@ def remove_assigned(state, char, positions):
         state.values[i-1] = -1
 
 
-def backtrack(char_dict, state, order):
+def backtrack(char_dict, state):
     print("-----------------------")
     values = state.values
     # check if done
@@ -384,7 +366,6 @@ def backtrack(char_dict, state, order):
 
     # select unassigned var
     char = select_var(char_dict, state)
-    # char = order[-1]
     print(char, char_dict[char].positions, char_dict[char].domain)
     # get domain val from var (can make var a node)
     domain_vals = char_dict[char].domain
@@ -393,16 +374,16 @@ def backtrack(char_dict, state, order):
             print('domain_vals', domain_vals)
             print('domain:', i)
             can_add_val(state, i, char_dict, char, 1) # apply changes to res_state
-            # order.pop()
-            backtrack(char_dict, state, order)
+            backtrack(char_dict, state)
+            # check if done
             flag = True
             for i in state.values:
                 if i == -1:
                     flag = False
                     break
+            # if all are assigned then should be correct since only add numbers that satisfy contraints
             if flag: return True
             positions = char_dict[char].positions
-            # print('removed')
             remove_assigned(state, char, positions)
     print('failed')
     return False
@@ -423,13 +404,18 @@ def main(file_name):
     for i in range(13):
         initial_values.append(-1)
     state.values = initial_values
-    order = ['E', 'O','D', 'S', 'M']
-    res = backtrack(char_dict, state, order)
+    res = backtrack(char_dict, state)
     print(res)
     print(state.assigned)
     values = state.values
-    print(values[0:4])
-    print(values[4:8])
-    print(values[8:13])
+    str_1 = ''
+    str_2 = ''
+    str_3 = ''
+    for i in values[0:4]: str_1 += str(i)
+    for i in values[4:8]: str_2 += str(i)
+    for i in values[8:13]: str_3 += str(i)
+    print(str_1)
+    print(str_2)
+    print(str_3)
 
-main('input1.txt')
+main('input2.txt')
